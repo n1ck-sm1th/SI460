f47038424aad88bb2a270722371fbb63b64fdafb
n1ck-sm1th <m265904@usna.edu>
2025-01-22 14:13:04 -0500

    test

--- lab/lab03/part4.py
+++ lab/lab03/part4.py
@@ -4,8 +4,15 @@
 from graphics import * 
 
 def main():
+    
+    #Point, normal, camera, and direction initialization.
     p = Point3D(1,1,1) 
-    print(p)   
+    n = Normal(-3, -3, -2)
+    camera = Point3D(1, 1, -10)
+    d = Vector3D(2, 2, 4)
+    
+    plane = Plane(p, n)
+    print(plane)   
     
 if __name__=="__main__":
     main()
